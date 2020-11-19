a1d58810b98aa8056ff702aa3043ce07cc7d20ba
hikaru426 <73151172+hikaru426@users.noreply.github.com>
2020-11-19 16:44:07 +0900

    バリデーション修正(presence: true,削除)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   has_many :items
 
   with_options presence: true do
-    with_options presence: true, format: { with: /(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]/, message: '半角英数字混合で入力してください' } do
+    with_options format: { with: /(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]/, message: '半角英数字混合で入力してください' } do
     validates :password
     end
     validates :nick_name
     validates :birthday
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
       validates :first_name
       validates :last_name
     end
-    with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
+    with_options format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナのみで入力して下さい' } do
       validates :first_name_kana
       validates :last_name_kana
     end
