36ab9362a5f70187c882b685d61972a670abf512
hikaru426 <73151172+hikaru426@users.noreply.github.com>
2020-11-21 21:10:39 +0900

    コントローラーの記述修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, ]
+  before_action :set_item,except:[:index,:new,:create]
   def index
     @items = Item.includes(:user).order(created_at: :desc) 
   end
@@ -18,18 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -42,5 +40,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:category_id, :condition_id, :delivery_id, :shipping_id, :prefecture_id, :image, :name, :info, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
